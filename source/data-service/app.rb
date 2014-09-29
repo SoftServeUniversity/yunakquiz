89884534935d5665747ff8d868bffe40bed56022
Tanya Filonych <tfilonych@mail.ru>
2014-09-29 22:14:28 +0300

    [WIP] Added Sqlite3 database and User migration. TODO: Fix Rakefile

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -1,15 +1,19 @@
 module PlastApp
   require 'sinatra'
+  require 'sinatra/activerecord'
   require 'json'
   require 'rest_client'
   require 'rubygems'
-  require 'mongo'
   require 'json/ext' # required for .to_json
-
   require 'sinatra/asset_pipeline'
+
+  class User < ActiveRecord::Base
+  end
 
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
+    
+    set :database, 'sqlite3:./db/quiz.db'
 
     get '/' do
       erb :index
