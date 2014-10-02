f9b281797509608e78880c3684f71429c735fb7f
OlegV <o.vitvitskyi@gmail.com>
2014-10-02 17:32:06 +0300

    [WIP] added classes for assessments

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -8,6 +8,24 @@ module PlastApp
 
   require 'sinatra/asset_pipeline'
 
+  class Measure < ActiveRecord::Base
+    has_many :assessments
+  end
+  
+  class Assessment < ActiveRecord::Base
+    has_many :questions
+    belongs_to :measure
+  end
+
+  class Question < ActiveRecord::Base
+    belongs_to :assessment
+    has_many :answers
+  end
+
+  class Answer < ActiveRecord::Base
+    belongs_to :question
+  end
+
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
 
