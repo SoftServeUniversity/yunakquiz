52e64a4be56675ec3b774caf2ff8e954cdbed7dc
Ivan-Uspensky <frey.terry@gmail.com>
2014-10-06 13:29:29 -0700

    app for registration of a new user

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -1,40 +1,59 @@
 module PlastApp
   require 'sinatra'
+  require 'sinatra/activerecord'
   require 'json'
   require 'rest_client'
   require 'rubygems'
-  require 'mongo'
   require 'json/ext' # required for .to_json
-
+  require 'sinatra/cross_origin'
   require 'sinatra/asset_pipeline'
-
+  
+  class User < ActiveRecord::Base
+  end
+  
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
+    register Sinatra::CrossOrigin
 
-    get '/' do
-      erb :index
+    configure do
+        enable :cross_origin
+        enable :sessions
+        set :session_secret, 'cca369ff55af5ceefc50939498d93f5905272422baf5d86dd0c4271e2e68a9ba'
+        set :allow_origin, :any
+        set :allow_methods, [:head, :get, :post, :options, :delete, :put]
+        set :allow_credentials, true
+        set :max_age, "86400"
+        set :allow_headers, ['*', 'Content-Type', 'Origin', 'Accept', 'X-Requested-With', 'x-xsrf-token', 'X-HTTP-Method-Override', 'Cache-Control']
+        set :expose_headers, ['Content-Type']
     end
-
-    get '/assessments' do
-      content_type :json
-      [{id: 1, name: 'assessment 1'}, {id: 2, name: 'assessment 2'}].to_json
+    
+    set :database, 'sqlite3:./db/quiz.db'
+    
+    options '/*' do
+        '*'
     end
-
-    put '/assessments' do
-      content_type :json
-      {response: 'Added an assessment'}.to_json
+    
+    post '/register' do
+        data = JSON.parse request.body.read
+        user = User.new
+        user.username = data['username'] 
+        user.first_name = data['first_name']
+        user.last_name = data['last_name']
+        user.hashed_password = data['password']
+        user.email = data['email']
+        user.birthday = data['birthday']
+        user.plast_hovel = data['plast_hovel']
+        user.plast_region = data['plast_region']
+        user.plast_level = data['plast_level']
+        user.picture = data['picture']
+      
+      if !User.find_by username: user.username
+        user.save
+        return [200, "ok"]
+      end
+        puts "this user already exists!"
+        return [409, "this user is already exists"]
     end
-
-    post '/assessments/:id' do
-      content_type :json
-      {response: "Updated to #{params['id']} assessment"}.to_json
-    end
-
-    delete '/assessments/:id' do
-      content_type :json
-      {response: "Assessment #{params['id']} has been deleted"}.to_json
-    end
-
+  
   end
-
 end
