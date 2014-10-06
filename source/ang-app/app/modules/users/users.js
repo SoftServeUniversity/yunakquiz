85ff0c76726587324058c9e7f83a159128296ae2
Tanya Filonych <tfilonych@mail.ru>
2014-10-07 00:03:48 +0300

    Added Register-related js file

--- source/ang-app/app/modules/users/users.js
+++ source/ang-app/app/modules/users/users.js
@@ -1,14 +1,22 @@
 'use strict';
 
 angular.module('yunakQuiz.users', ['ngRoute'])
-
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/auth/signup', {
     templateUrl: 'modules/users/users_new.html',
-    controller: 'UsersCtrl'
+    controller: 'RegistrationController',
+    controllerAs: "reg"
   });
 }])
-
-.controller('UsersCtrl', [function() {
-
+.controller("RegistrationController", ["$http", "$location", function($http, $location){
+    this.user = {};
+    this.cancel = function(){
+        $location.path("/");
+    };
+    this.submitRegistration = function(){
+        $http.post("http://localhost:9292/register", this.user)
+        .success(function(data){
+            $location.path("/");
+        });
+    };
 }]);
