fc8236365b4368b03be96156483a4f6fc33411df
Ivan-Uspensky <frey.terry@gmail.com>
2014-10-06 13:48:52 -0700

    app for login and session of authorization

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -1,40 +1,62 @@
 module PlastApp
   require 'sinatra'
+  require 'sinatra/activerecord'
   require 'json'
   require 'rest_client'
   require 'rubygems'
-  require 'mongo'
   require 'json/ext' # required for .to_json
-
+  require 'sinatra/cross_origin'
   require 'sinatra/asset_pipeline'
-
+  
+  class User < ActiveRecord::Base
+  end
+  
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
+    register Sinatra::CrossOrigin
 
-    get '/' do
-      erb :index
+    configure do
+      	enable :cross_origin
+      	enable :sessions
+      	set :session_secret, 'cca369ff55af5ceefc50939498d93f5905272422baf5d86dd0c4271e2e68a9ba'
+      	set :allow_origin, :any
+      	set :allow_methods, [:head, :get, :post, :options, :delete, :put]
+      	set :allow_credentials, true
+      	set :max_age, "86400"
+      	set :allow_headers, ['*', 'Content-Type', 'Origin', 'Accept', 'X-Requested-With', 'x-xsrf-token', 'X-HTTP-Method-Override', 'Cache-Control']
+      	set :expose_headers, ['Content-Type']
+    end
+    
+    set :database, 'sqlite3:./db/quiz.db'
+    
+    options '/*' do
+      	'*'
+    end
+    
+    get '/access' do
+      	if session[:user_id]
+        	user = User.find(session[:user_id])
+        	return [200, user.username]
+    	end
+      		return [401, "unauthorized"]
     end
 
-    get '/assessments' do
-      content_type :json
-      [{id: 1, name: 'assessment 1'}, {id: 2, name: 'assessment 2'}].to_json
+    post '/login' do
+      	data = JSON.parse request.body.read
+      	user = User.where(username: data['username'], hashed_password: data['password']).first 
+      	if !user.nil?
+        	if data['remember']
+          		session[:user_id] = user.id
+        	end
+        		return [200, user.username]
+      	end
+      		return [401, "unauthorized"]
     end
 
-    put '/assessments' do
-      content_type :json
-      {response: 'Added an assessment'}.to_json
+    get '/logout' do
+      	session.delete(:user_id)
+      	return [200, "ok"]
     end
-
-    post '/assessments/:id' do
-      content_type :json
-      {response: "Updated to #{params['id']} assessment"}.to_json
-    end
-
-    delete '/assessments/:id' do
-      content_type :json
-      {response: "Assessment #{params['id']} has been deleted"}.to_json
-    end
-
-  end
-
+  
+  	end
 end
