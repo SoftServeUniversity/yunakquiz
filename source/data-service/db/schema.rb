af1bf8252b638e77e954804ce18af3c436f8b4ef
Measim <l_eugene@inbox.ru>
2014-10-14 00:42:24 +0300

    splited migration files each table - separate file

--- source/data-service/db/schema.rb
+++ source/data-service/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010180812) do
+ActiveRecord::Schema.define(version: 20141013210013) do
 
   create_table "answers", force: true do |t|
     t.integer "question_id"
