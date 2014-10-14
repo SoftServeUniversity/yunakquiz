2666a0d5d1cb8c5d5965cee9c5748f05d9286328
Ivan-Uspensky <frey.terry@gmail.com>
2014-10-14 01:39:32 -0700

    update app.rb according to new model type

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -8,9 +8,6 @@ module PlastApp
   require 'sinatra/cross_origin'
   require 'sinatra/asset_pipeline'
 
-  class User < ActiveRecord::Base
-  end
-  
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
 
@@ -44,24 +41,13 @@ module PlastApp
     end
     
     post '/register' do
-        data = JSON.parse request.body.read
-        user = User.new
-        user.username = data['username'] 
-        user.first_name = data['first_name']
-        user.last_name = data['last_name']
-        user.hashed_password = data['password']
-        user.email = data['email']
-        user.birthday = data['birthday']
-        user.plast_hovel = data['plast_hovel']
-        user.plast_region = data['plast_region']
-        user.plast_level = data['plast_level']
-        user.picture = data['picture']
-      
-      if !User.find_by username: user.username
-        user.save
+      data = JSON.parse request.body.read
+      user = User.new(data)
+      if user.save
         return [200, "ok"]
-      end
+      else
         return [400, user.errors.messages.to_json]
+      end
     end
   
   end
