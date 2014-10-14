268e2702c6b1320d0ee3c4e45e37720c4fd3dc8e
Flur <superflur@gmail.com>
2014-10-14 14:55:36 +0300

    Not big changes, just testing

--- source/ang-app/app/modules/guest-search/guest-search.js
+++ source/ang-app/app/modules/guest-search/guest-search.js
@@ -13,27 +13,32 @@
       	}
     ]);      
 
+//==============================================================================
+    
     //Ctrl for guest-search-page
     app.controller('SearchCtrl', function SearchCtrl($scope, $http, searchFactory){
 
       //Creating the filter for search
+
       $http.get("http://localhost:9292/guest-search")
       .success(function(data){
-        $scope.allCats = data;
-        $scope.keyWord="teg1"; 
-        searchFactory($scope.allCats, $scope.keyWord);  
+        $scope.allCats = data;  
+        //searchFactory($scope.allCats, $scope.keyWord);
       });
-      
+
     });
 
-    //Factory that sends post request and return search results based on input data
+//==============================================================================
+    
+    //Factory that recives 2 arrays of tegs and categories
+    //and sends post request and return search results based on input data
     app.factory('searchFactory', function($http){
       return function(allCats, keyWord){
         //place for loop to clean allcats object
 
         var searchRequest = {
-          tags: ['teg2'],
-          categories: [1,2,3,4,6,7,8,9,10,11,12] 
+          tags: ['teg1'],
+          categories: [1,2,3,4,5,6,7,8,9,10,11] 
         };
         //searchRequest.allCats[8].search = true; // just for testing
         //searchRequest.allCats[1].search = true; // just for testing
