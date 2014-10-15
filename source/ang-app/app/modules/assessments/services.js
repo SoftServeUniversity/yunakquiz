ac13f29f8d29f479e1e50a6d5abe028cd941894d
OlegV <o.vitvitskyi@gmail.com>
2014-10-15 17:41:24 +0300

    [WIP] Added put and post methods in service QuizData

--- source/ang-app/app/modules/assessments/services.js
+++ source/ang-app/app/modules/assessments/services.js
@@ -3,36 +3,27 @@
 /** Connection to back-end for quiz  */
 yunakQuizApp.factory('QuizData', ['$http', function($http){
     return{
-        get: function(id, callback){
+      get: function(id, callback){
           $http({ method: 'GET', url: 'http://localhost:9292/assessments/'+id })
-          .success(function(data, status, headers, config) {
-				callback(data); 
-			});
-        },
-        save: function(quiz){
+            .success(function(data, status, headers, config) {
+				      callback(data); 
+			       });
+      },
+      save: function(quiz){
           if (quiz.id) { 
-            $http({
-                method: 'POST',
-                url: 'http://localhost:9292/admin/assessments/'+quiz.id,
-                data: quiz,
-                headers: {
-                    'Content-Type': 'application/json; charset=utf-8'
-                }})
-              .success(function(data, status, headers, config) {
-                  
-              });
-            }
+            $http.put("http://localhost:9292/admin/assessments/"+quiz.id, quiz)
+             .success(function(data, status, headers, config) {})
+             .error( function(data, status, headers, config) {});
+          }
           else { 
             $http.post('http://localhost:9292/admin/assessments', quiz)
-              .success(function(data, status, headers, config) {
-    
-                })
-              .error(function(data, status, headers, config) {
-                });
-        }}, 
-        quiz:{}
-      }
+              .success(function(data, status, headers, config) {})
+              .error(function(data, status, headers, config) {});
+          }
+      }, 
+      quiz:{}
     }
-  ]);
+  }
+]);
 
 
