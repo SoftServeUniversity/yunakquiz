7fa1df8cfc908333097e54c3ba0d4e2163309361
IRRAT <kaikai88ss@gmail.com>
2014-10-16 07:04:36 +0300

    Base template created

---
+++ source/ang-app/app/modules/personalCabinet/personalCabinet.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('yunakQuiz.personalCabinet', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/personalCabinet', {
+    templateUrl: 'modules/personalCabinet/personalCabinet_index.html',
+    controller: 'PersonalCabinetCtrl'
+  });
+}])
+
+.controller('PersonalCabinetCtrl', [function() {
+
+}]);
