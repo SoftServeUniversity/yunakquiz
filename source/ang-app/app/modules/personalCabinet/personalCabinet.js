d08d6e386a9b073c88615a46eab70a6112c7407d
IRRAT <kaikai88ss@gmail.com>
2014-10-23 01:02:28 +0300

    reconstructed using ng-view

--- source/ang-app/app/modules/personalCabinet/personalCabinet.js
+++ source/ang-app/app/modules/personalCabinet/personalCabinet.js
@@ -3,12 +3,45 @@
 angular.module('yunakQuiz.personalCabinet', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/personalCabinet', {
-    templateUrl: 'modules/personalCabinet/personalCabinet_index.html',
-    controller: 'PersonalCabinetCtrl'
-  });
+  $routeProvider
+  			.when('/personal_cabinet/published', {
+	    		templateUrl: 'modules/personalCabinet/published.html',
+	    		controller: 'PublishedCtrl'
+  			})
+  			.when('/personal_cabinet/unfinished', {
+	    		templateUrl: 'modules/personalCabinet/unfinished.html',
+	    		controller: 'UnfinishedCtrl'
+  			})
+  			.when('/personal_cabinet/unapproved', {
+	    		templateUrl: 'modules/personalCabinet/unapproved.html',
+	    		controller: 'UnapprovedCtrl'
+  			})
+  			.when('/personal_cabinet/created', {
+	    		templateUrl: 'modules/personalCabinet/created.html',
+	    		controller: 'CreatedCtrl'
+  			})
+  			.when('/personal_cabinet/profile', {
+	    		templateUrl: 'modules/personalCabinet/profile.html',
+	    		controller: 'ProfileCtrl'
+  			});
 }])
 
-.controller('PersonalCabinetCtrl', [function() {
+.controller('PublishedCtrl', [function() {
+
+}])
+.controller('UnfinishedCtrl', [function() {
+
+}])
+.controller('UnapprovedCtrl', [function() {
+
+}])
+
+.controller('CreatedCtrl', [function() {
+
+}])
+
+.controller('ProfileCtrl', [function() {
 
 }]);
+
+
