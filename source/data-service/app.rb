c97f60859990d811639810c3673e809b9d1b18af
OlegV <o.vitvitskyi@gmail.com>
2014-10-23 17:33:26 +0300

    Added migrations and models for Quiz DataBase

--- source/data-service/app.rb
+++ source/data-service/app.rb
@@ -9,33 +9,58 @@ module PlastApp
 
   require 'sinatra/asset_pipeline'
 
-  
   class YunakQuiz < Sinatra::Base
     register Sinatra::AssetPipeline
-
     register Sinatra::ActiveRecordExtension
     register Sinatra::CrossOrigin
+
     Dir.glob('./config/*.rb').each {|file| require file}
     Dir.glob('./models/*.rb').each {|file| require file}
 
+
     get '/' do
         erb :index
+    end
+
+    options '/*' do
     end
 
     get '/assessments' do
       content_type :json
       [{id: 1, name: 'assessment 1'}, {id: 2, name: 'assessment 2'}].to_json
     end
-
-    put '/assessments' do
+       
+    put '/admin/assessments/:id' do
       content_type :json
-      {response: 'Added an assessment'}.to_json
+      data = JSON.parse(request.body.read)
+      quiz = Quiz.updateQ(data)
+      if quiz
+        return [200, quiz.id.to_json]
+      else
+        return [400, quiz.errors.messages.to_json]
+      end    
     end
 
-    post '/assessments/:id' do
-      cross_origin
+    post '/admin/assessments' do
       content_type :json
-      {response: "Updated to #{params['id']} assessment"}.to_json
+      data = JSON.parse(request.body.read)
+      #check permisions here
+      quiz = Quiz.createQ(data)
+      if quiz
+        return [200, quiz.id.to_json]
+      else
+        return [400, quiz.errors.messages.to_json]
+      end
+    end
+
+    get '/assessments/:id' do
+      content_type :json
+      quiz = Quiz.queryQ(params['id'])
+      if quiz['id']
+        JSON.pretty_generate(quiz) 
+      else
+        return [400, quiz.to_json]
+      end
     end
 
     delete '/assessments/:id' do
---
+++ source/data-service/db/seeds.rb
@@ -0,0 +1,23 @@
+#encoding: utf-8 
+
+Category.create(title: "Спорт" ,category_id: 0)
+Category.create(category_id: 1, title: "Футбол")
+Category.create(category_id: 1, title: "Хокей")
+Category.create(category_id: 0, title: "Історія")
+Category.create(category_id: 4, title: "Історія України")
+Category.create(category_id: 4, title: "Історія світу")
+Quiz.create(category_id: 2, title: "Тест на знання правил футболу", description: "Детальний опис тесту")
+Question.create(quiz_id: 1, title: "Скільки гравців в команді?", description: "Згідно правил на полі знаходиться 11 гравців однієї команди")
+Answer.create(question_id: 1, title: "11", correct: true)
+Answer.create(question_id: 1, title: "12", correct: false)
+Answer.create(question_id: 1, title: "5", correct: false)
+Question.create(quiz_id: 1, title: "Скільки триває один тайм?", description: "Один тайм матчу триває 45 хв. не враховуючи додаткового часу")
+Answer.create(question_id: 2, title: "20хв", correct: false)
+Answer.create(question_id: 2, title: "45хв", correct: true)
+Answer.create(question_id: 2, title: "до останнього гравця", correct: false)
+Question.create(quiz_id: 1, title: "Що відбудеться, коли гравець торкнеться м’яча рукою?", description: "Це є порушенням правил, після того як гравець торкнувся мяча рукою гра зупиняється, та назначається штрафний")
+Answer.create(question_id: 3, title: "Порушенння правил", correct: true)
+Answer.create(question_id: 3, title: "Штрафний удар", correct: true)
+Answer.create(question_id: 3, title: "Дадуть пиріжок", correct: false)
+Answer.create(question_id: 3, title: "Дадуть в голову", correct: false)
+
