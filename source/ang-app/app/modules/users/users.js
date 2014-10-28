c8e68b22d5a396ae213e3316ea92dfa1977bbcbd
IRRAT <kaikai88ss@gmail.com>
2014-10-28 14:46:04 +0200

    some conflicts resolved

--- source/ang-app/app/modules/users/users.js
+++ source/ang-app/app/modules/users/users.js
@@ -12,7 +12,7 @@ angular.module('yunakQuiz.users', ['ngRoute', 'ngResource'])
 .controller("RegistrationController", ["userService", "$location", "$scope", "userValidationService", "existUser",
   function(userService, $location, $scope, userValidationService, existUser){
     this.user = {};
-    this.validation = new $userValidation($scope);
+    this.validation = new userValidationService($scope);
     this.emailPattern = /^[\w+\.]?\w+@\w+\.[a-z]{2,4}$/;
     this.getRandom = function(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
