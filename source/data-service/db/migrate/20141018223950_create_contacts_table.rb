4860b494049ef8ac677f1133409156b6d0075b5e
Measim <l_eugene@inbox.ru>
2014-10-29 22:01:43 +0200

    few identations fix

--- source/data-service/db/migrate/20141018223950_create_contacts_table.rb
+++ source/data-service/db/migrate/20141018223950_create_contacts_table.rb
@@ -1,10 +1,10 @@
 class CreateContactsTable < ActiveRecord::Migration
-	def change
-		create_table :contacts do |table|
-			table.string :role
-			table.string :phone
-			table.string :mail
-			table.string :address
-		end
-	end
+  def change
+    create_table :contacts do |table|
+      table.string :role
+      table.string :phone
+      table.string :mail
+      table.string :address
+    end
+  end
 end
