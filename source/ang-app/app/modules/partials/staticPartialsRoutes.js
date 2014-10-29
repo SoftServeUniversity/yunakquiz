aa711e0e9ca8049b5a49c5094e4177ba9b6bd14f
Measim <l_eugene@inbox.ru>
2014-10-29 21:57:00 +0200

    identation fixed

--- source/ang-app/app/modules/partials/staticPartialsRoutes.js
+++ source/ang-app/app/modules/partials/staticPartialsRoutes.js
@@ -1,15 +1,16 @@
 (function (){
-	var  app = angular.module('yunakQuiz.staticPartialsRoute' ,['ngRoute']);
+  var  app = angular.module('yunakQuiz.staticPartialsRoute' ,['ngRoute']);
 
-		app.config(['$routeProvider',
-      		function($routeProvider) {
-        		$routeProvider.
-            	when('/contacts', {
-              	templateUrl: './modules/partials/contacts-page.html',
-                controller:'contacts'
-            	})
-      		}
+    app.config(['$routeProvider',
+      function($routeProvider) {
+        $routeProvider.
+          when('/contacts', {
+            templateUrl: './modules/partials/contacts-page.html',
+            controller:'contacts'
+          })
+      }
     ]);
+
     app.controller('contacts', 
       ['$scope', '$http', function ($scope, $http) {
         $scope.contacts = {};
