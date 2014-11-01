59089180622f8bd6ec6948093a8a832e04046296
IRRAT <kaikai88ss@gmail.com>
2014-11-01 20:26:18 +0200

    nothing special

--- source/ang-app/e2e-tests/tests/mocked-backend.js
+++ source/ang-app/e2e-tests/tests/mocked-backend.js
@@ -1,11 +1,12 @@
 exports.httpBackendMock = function() {
 	angular.module('httpBackendMock', ['yunakQuiz', 'ngMockE2E'])
     .run(function($httpBackend) {
-    	$httpBackend.whenPOST("http://localhost:9292/register", {username: 'Inokentiy', password: '12345678', password_confirmation: '12345678', email: 'ewrrg@fgfgf', birthday: '2014-10-08'})
-    	.respond([400, {"username":["has already been taken"]}]);
+    // 	$httpBackend.whenPOST("http://localhost:9292/register", {username: 'Inokentiy', password: '12345678', password_confirmation: '12345678', email: 'ewrrg@fgfgf', birthday: '2014-10-08'})
+    // 	.respond([400, {"username":["has already been taken"]}]);
     
-    $httpBackend.whenGET(/modules\/\w+.*/).passThrough();
-    $httpBackend.whenGET(/^\w+.*/).passThrough();
-	$httpBackend.whenGET().passThrough();
+    // $httpBackend.whenGET(/modules\/\w+.*/).passThrough();
+    // $httpBackend.whenGET(/^\w+.*/).passThrough();
+    // $httpBackend.whenGET(/localhost:8000\/\w+.*/).passThrough();
+    
   });
 };
