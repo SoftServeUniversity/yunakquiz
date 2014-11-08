282707d7b3f9993f551753fbdd972ed0783558f2
Measim <l_eugene@inbox.ru>
2014-11-08 13:44:01 +0200

    minor fix in migtration

--- source/data-service/db/migrate/20141004193036_create_questions_table.rb
+++ source/data-service/db/migrate/20141004193036_create_questions_table.rb
@@ -1,10 +1,10 @@
 class CreateQuestionsTable < ActiveRecord::Migration
-	def change
-		create_table :questions do |table|
-			table.belongs_to :quiz
-			table.string :title
-			table.text :description, :limit => 200
-			table.timestamps
-		end
-	end
+  def change
+    create_table :questions do |table|
+      table.belongs_to :quiz
+      table.string :title
+      table.text :description
+      table.timestamps
+    end
+  end
 end
