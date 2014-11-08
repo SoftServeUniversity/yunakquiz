7b2360b28fb6bac1951fcde23f4a46e7fb924338
Measim <l_eugene@inbox.ru>
2014-11-08 03:55:27 +0200

    minor changes, identations fix, factory moved to admin panel

--- source/ang-app/app/modules/partials/staticPartialsRoutes.js
+++ source/ang-app/app/modules/partials/staticPartialsRoutes.js
@@ -1,5 +1,5 @@
 (function (){
-  var  app = angular.module('yunakQuiz.staticPartialsRoute' ,['ngRoute']);
+  var  app = angular.module('yunakQuiz.staticPartialsRoute' ,['ngRoute','yunakQuiz.aboutusTab']);
 
     app.config(['$routeProvider',
       function($routeProvider) {
@@ -12,12 +12,11 @@
     ]);
 
     app.controller('aboutUs', 
-      ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+      ['$scope', '$http', '$sce', 'aboutUsReadUpdate', function ($scope, $http, $sce, aboutUsReadUpdate) {
         $scope.about_Us = '';
 
-        $http.get('http://localhost:9292/about_us').success(function(data){
+        aboutUsReadUpdate.read().success(function(data){
           $scope.about_Us = $sce.trustAsHtml(data[0].about_us);
         });    
     }])
 })(); 
-
