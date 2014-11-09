0482caa2e4d2e3e2c35b51dad7cd8103a4cb0684
Ivan-Uspensky <frey.terry@gmail.com>
2014-11-09 13:54:35 -0800

    q defer implemented for factory

--- source/ang-app/app/modules/admin/admin.js
+++ source/ang-app/app/modules/admin/admin.js
@@ -10,128 +10,97 @@ angular.module('yunakQuiz.admin', ['ngRoute'])
 }])
 
 .constant('tabs', [
-  {name :   'admin1',
-   temp :   '.modules/admin/admin-1.html',
-   caption:   'Admin Tab N1' 
-  },
+	{name : 	'admin1',
+	 temp : 	'.modules/admin/admin-1.html',
+	 caption: 	'Admin Tab N1' 
+	},
 
-  {name :   'admin2',
-   temp :   '.modules/admin/admin-2.html',
-   caption:   'Admin Tab No 2'
-  },
+	{name : 	'admin2',
+	 temp : 	'.modules/admin/admin-2.html',
+	 caption: 	'Admin Tab No 2'
+	},
 
-  {name :   'admin3',
-   temp :   '.modules/admin/admin-3.html',
-     caption:   'Admin Tab # 3'
-  },
+	{name : 	'admin3',
+	 temp : 	'.modules/admin/admin-3.html',
+     caption: 	'Admin Tab # 3'
+	},
 
-  {name :   'admin4',
-   temp :   '.modules/admin/admin-4.html',
-     caption:   'Admin Tab Mumber Four'
-  },
+	{name : 	'admin4',
+	 temp : 	'.modules/admin/admin-4.html',
+     caption: 	'Admin Tab Mumber Four'
+	},
 
-  {name :   'moder1',
-   temp :   '.modules/admin/moder-1.html',
-     caption:   'Moderator Tab Number One'
-  },
+	{name : 	'moder1',
+	 temp : 	'.modules/admin/moder-1.html',
+     caption: 	'Moderator Tab Number One'
+	},
 
-  {name :   'moder2',
-   temp :   '.modules/admin/moder-2.html',
-     caption:   'Moder Tab n 2'
-  },
+	{name : 	'moder2',
+	 temp : 	'.modules/admin/moder-2.html',
+     caption: 	'Moder Tab n 2'
+	},
 
-  {name :   'moder3',
-   temp :   '.modules/admin/moder-3.html',
-   caption:   'Moder Tab nO 3'
-  }
+	{name : 	'moder3',
+	 temp : 	'.modules/admin/moder-3.html',
+ 	 caption: 	'Moder Tab nO 3'
+	}
 
-  // ,{name :   'comm1',
-  //  temp :  '.modules/admin/comm-1.html',
-    //   caption:   'ANYTHING CAN BE HERE'
-  // },
+	// ,{name : 	'comm1',
+	//  temp : 	'.modules/admin/comm-1.html',
+ 	// 	 caption: 	'ANYTHING CAN BE HERE'
+	// },
 
-  // {name :  'comm2',
-  //  temp :  '.modules/admin/comm-2.html',
-  //  caption:  'SOMETHING ELSE IS HERE'
-  // },
+	// {name : 	'comm2',
+	//  temp : 	'.modules/admin/comm-2.html',
+	//  caption: 	'SOMETHING ELSE IS HERE'
+	// },
 
-  // {name :  'comm3',
-  //  temp :  '.modules/admin/comm-3.html',
-  //  caption:  'EXTREMELY DIFFERENT LAYS HERE'
-  // }
+	// {name : 	'comm3',
+	//  temp : 	'.modules/admin/comm-3.html',
+	//  caption: 	'EXTREMELY DIFFERENT LAYS HERE'
+	// }
 ])
 
-
-.factory('getBackEnd', ["$location", "$http", "tabs", function($location, $http, tabs) {
-  function getData(){
-    console.log("inside getData: ");
-    return $http.get("http://localhost:9292/admin")
-  }
-  return {
-    getAccess: function(){
-      console.log("inside getAccess: ");
-      getData().success(function(data){
-        console.log("inside getData success: ");
-        var result = [];
-        var givenTabs = tabs;
-        var userAccess = data;
-        
-        var i=0;
-        var j=0;
-        var tlen = givenTabs.length;
-        var alen = userAccess.length;
-        for (j; j < alen; j++) {
-          for (i; i < tlen; i++) {
-            if(givenTabs[i].name == userAccess[j]){
-              result.push([givenTabs[i].name, givenTabs[i].temp, givenTabs[i].caption]);
-            }
-          };
-          i = 0;
-        };
-        console.log("afer cycle: " + result);
-        return result;
-      })
-    }
-  }
-  // var myPromise = $http.get("http://localhost:9292/admin");
- //     var myObj = {
- //       promise : myPromise,
- //       data : {}
- //     }
-  // myPromise.then(function(response) {
- //     var result = [];
-  //  var givenTabs = tabs;
-  //  var userAccess = response.data;
+.factory('getTabTemplates', ["$location", "$http", "tabs", "$q", function($location, $http, tabs, $q) {
     
-  //  var i=0;
-  //  var j=0;
-  //  var tlen = givenTabs.length;
-  //  var alen = userAccess.length;
-  //  for (j; j < alen; j++) {
-  //    for (i; i < tlen; i++) {
-  //      if(givenTabs[i].name == userAccess[j]){
-  //        result.push([givenTabs[i].name, givenTabs[i].temp, givenTabs[i].caption]);
-  //      }
-  //    };
-  //    i = 0;
-  //  };
-  //  // console.log("result afret cycle: " + result);
-  //  myObj = {data: result};
- //     console.log("after for: " + myObj.data);
- //     // return myObj;
- //    });
- //    // console.log("after then: " + myObj.data);
- //     return myObj;
+    return {
+    	getResponse: function(){
+    		var defer = $q.defer();
+    		var result = [];
+    		$http.get("http://localhost:9292/admin")
+    			.success(function(data){
+	    			var givenTabs = tabs;
+					var userAccess = data;
+					var i=0;
+					var j=0;
+					var tlen = givenTabs.length;
+					var alen = userAccess.length;
+					for (j; j < alen; j++) {
+						for (i; i < tlen; i++) {
+							if(givenTabs[i].name == userAccess[j]) {
+								result.push([givenTabs[i].name, givenTabs[i].temp, givenTabs[i].caption]);
+							}			
+						};
+						i = 0;
+					};
+    				defer.resolve(result);
+    			});
+    		return defer.promise;			
+    	}
+    }
 
 }])
 
+.controller("AdminCtrl", ["$location", "$scope", "$http", 'getTabTemplates', function($location, $scope, $http, getTabTemplates){
+	
+    getTabTemplates.getResponse().then(function(asd){
+    	console.log(asd);
+    	if (asd.length > 0)
+    		$scope.results = asd;
+    	else		
+    		$location.path( "/404" );
+			// console.log("redirect needed");
+    });
 
-
-.controller("AdminCtrl", ["$location", "$scope", "$http", 'getBackEnd', function($location, $scope, $http, getBackEnd){
-  var asd; 
-  console.log("begin: ");
-  asd = getBackEnd.getAccess.resolved();
-  // asd = getBackEnd.getAccess();
-  console.log("after begin: " + asd);
-  $scope.results = asd;
-}])
+}
+])
