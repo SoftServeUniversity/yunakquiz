2395dc486854f40a80dc2d2db19058de5ef196a0
anabnk <anastasia.buniak@gmail.com>
2014-11-10 13:54:59 +0200

    front-end changes for subcategory search

--- source/ang-app/app/app.js
+++ source/ang-app/app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('yunakQuiz', [
   'ngRoute',
+  'ui.bootstrap',
   'yunakQuiz.measures',
   'yunakQuiz.assessments',
   'yunakQuiz.users',
