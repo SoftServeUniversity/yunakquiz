736b60ddf2fa8fb0cd6d41371cd9190b364e945b
OlegV <o.vitvitskyi@gmail.com>
2014-11-18 11:42:22 +0200

    [WIP] RevieQuiz refactoring

--- source/ang-app/app/modules/moderationCabinet/ReviewCtrl.js
+++ source/ang-app/app/modules/moderationCabinet/ReviewCtrl.js
@@ -1,91 +1,25 @@
 'use strict';
 /** Quiz Edit controller  */
-yunakQuizApp.controller('ReviewCtrl', ['$scope','QuizData', '$routeParams', '$location','QuizValidation', function($scope, QuizData, $routeParams, $location, QuizValidation) {
+yunakQuizApp.controller('ReviewCtrl', 
+  ['$scope','QuizResourceService', 'QuizCommentsService', '$routeParams', '$location','QuizDataService', 
+  function($scope, QuizResourceService, QuizCommentsService, $routeParams, $location, QuizDataService) {
 
   /** get Quiz by ID */
-  $scope.init = function(){
-    QuizData.getForEdit($routeParams.quiz_id)
+  QuizResourceService.get($routeParams.quiz_id)
       .success(function(data, status, headers, config){
-        $scope.quiz = data;
-        $scope.getComments(data['id']);
-        $scope.getCat();
+         $scope.quiz = data;
+         getComments(data['id']);
       })
       .error(function(data){
         $location.path('/404/');
       });
-    }
+    
 
-  $scope.getCat = function(){
-    QuizData.getCat().success(function(data, status, headers, config) {
-          $scope.categories=data;      
-          $scope.selectCat();  
-      });
-  };
-
-  $scope.getComments = function(quiz_id) {
-    QuizData.getComments(quiz_id)
+  function getComments(quiz_id) {
+    QuizCommentsService.get(quiz_id)
       .success(function(data, status, headers, config){
         $scope.comments = data;
       });
-  };
-
-  $scope.selectCat = function(){
-    for (var i=0; i < $scope.categories.length; i++){   
-      if ($scope.selectSubcat($scope.categories[i].categories)){
-        $scope.selectedCat = $scope.categories[i];
-      }
-    };
-  }
-
-  $scope.selectSubcat = function(subCatsArray){
-    for (var i=0; i < subCatsArray.length; i++){    
-      if (subCatsArray[i].id == $scope.quiz.category_id){
-        $scope.selectedSubcat = subCatsArray[i];
-        return true;
-      };
-    };
-  };
-
-  $scope.clearSubcat = function(){
-    $scope.selectedSubcat='';
-  };
-
-  /** get all tags from backend*/
-  $scope.loadTags = function(query) {
-      return QuizData.getTags(query)
-  };
-
-
-  /** add empty answer*/
-  $scope.addAnswer = function(question) {
-    question.answers.push({correct:false});
-  };
-
-  /** mark answer to delete in backend */
-  $scope.deleteAnswer = function(answer) {
-    answer.toDelete = true;
-  }
-
-  /** set that this answer to be correct/incorrect */
-  $scope.setCorrectAnswer=function(question,answer){
-    question.invalid = false;
-    answer.correct = !answer.correct;
-  };
-
-  /** add empty question */
-  $scope.addQuestion = function(){
-    $scope.addQuestionDisabled = true;
-    $scope.quiz.questions.push({answers:[{correct:false},{correct:false}]});
-  };
-    
-  /** delete question or mark to delete it on backend */  
-  $scope.deleteQuestion = function(question,index){
-    if(question.id){
-      question.toDelete = true;
-    }
-    else{
-      $scope.quiz.questions.splice(index,1);
-    }
   };
 
   $scope.deleteComment=function(index){
@@ -97,26 +31,44 @@ yunakQuizApp.controller('ReviewCtrl', ['$scope','QuizData', '$routeParams', '$lo
     $scope.comment='';
   };
 
+  function sendComments(state){
+  var service;
+  if (state === "published") {
+    service = QuizCommentsService.delete($scope.quiz.id);
+  }
+  else {
+   service = QuizCommentsService.update($scope.comments); 
+  }
+  service
+    .success(function(data, status, headers, config) {
+      $location.path('/admin/moderationCabinet/');
+    })
+    .error(function() {
+      window.scrollTo(0,0);
+      $scope.errorMsg = 'Ваш тест не збережено';
+    })
+
+  };
+    
   /** save draft Quiz */
   $scope.enhanceQuiz=function(){
-    $scope.sendQuiz("enhance");
+    sendQuiz("enhance");
   };
 
   /** save Quiz for review */
   $scope.publishQuiz=function(){
-    $scope.sendQuiz("published");
+    sendQuiz("published");
   };
 
   /** send Quiz to backend  */
-  $scope.sendQuiz = function(state){
-    $scope.quiz.category_id = $scope.selectedSubcat.id;
+  function sendQuiz(state){
     $scope.quiz.status = state;
         
-    if(!QuizValidation($scope.quiz.questions)){
-        
-      QuizData.update($scope.quiz)
+    if(!QuizDataService.validateQuiz($scope.quiz)){
+  
+      QuizResourceService.update($scope.quiz)
         .success(function(data, status, headers, config) {
-            $scope.sendComments($scope.quiz.status);
+          sendComments($scope.quiz.status);
         })
         .error(function() {
             window.scrollTo(0,0);
@@ -124,18 +76,6 @@ yunakQuizApp.controller('ReviewCtrl', ['$scope','QuizData', '$routeParams', '$lo
         })
     }
   };
-  $scope.sendComments = function(state){
-   var service = (state=="published") ? QuizData.deleteComments($scope.quiz.id) : QuizData.updateComments($scope.comments);
-    service
-      .success(function(data, status, headers, config) {
-        $location.path('/admin/moderationCabinet/');
-      })
-      .error(function() {
-        window.scrollTo(0,0);
-        $scope.errorMsg = 'Ваш тест не збережено';
-      })
-  };
-    
-  $scope.init();
 
+  
 }]);
