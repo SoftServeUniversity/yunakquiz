5eb23c44cdd1d532328067b931bd966d458b40b1
OlegV <o.vitvitskyi@gmail.com>
2014-11-20 14:54:00 +0200

    added results table

--- source/data-service/models/quiz.rb
+++ source/data-service/models/quiz.rb
@@ -3,6 +3,7 @@ class Quiz < ActiveRecord::Base
   belongs_to :user
   has_many :questions
   has_many :comments
+  has_many :results
   has_and_belongs_to_many :tags
   enum status: [:draft, :review, :enhance, :published, :deleted]
 
--- source/data-service/models/user.rb
+++ source/data-service/models/user.rb
@@ -3,6 +3,7 @@ require 'protected_attributes'
 
 class User < ActiveRecord::Base
   has_many :quizzes
+  has_many :results
   belongs_to :role
   attr_protected :hashed_password, :salt
   attr_accessor :password
