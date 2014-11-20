f10f4e8ee7bfbbde64eb5ca11a7bba9cd51ec0b4
Flur <superflur@gmail.com>
2014-11-20 15:25:43 +0200

    Tests added

--- source/ang-app/e2e-tests/tests/guest-search-test.js
+++ source/ang-app/e2e-tests/tests/guest-search-test.js
@@ -4,39 +4,39 @@ describe('Guest search page', function() {
 
   var ptor = protractor.getInstance();
   var mockModule = require('../guest-search-mocked-backend.js');
-  
 
-  var category1 = {"id":1,"category_id":0,"title":"Спорт"};
-  var subcategory1 = {"id":2,"category_id":1,"title":"Футбол"};
-  var subcategory2 = {"id":3,"category_id":1,"title":"Хокей"};
-
-  var category2 = {"id":4,"category_id":0,"title":"Історія"};
-  var subcategory3 = {"id":5,"category_id":4,"title":"Історія України"};
-  var subcategory4 = {"id":6,"category_id":4,"title":"Історія світу"};
-
-  var category3 = {"id":7,"category_id":0,"title":"Програмування"};
-  var subcategory5 = {"id":8,"category_id":7,"title":"Основи ООП"};
-  var subcategory6 = {"id":9,"category_id":7,"title":"Основи Java"};
+  var searchButton = element(by.buttonText('Пошук'));
+  var applyFilterButton = element(by.buttonText('Застосувати фільтр')); 
+  var input = element(by.model('newTag.text'));
+  var firstResult = element.all(by.repeater('quiz in searchResults')).get(0);
+  var secondResult = element.all(by.repeater('quiz in searchResults')).get(1);
 
   beforeEach(function() {  
     ptor.addMockModule('httpBackendMock', mockModule.httpBackendMock);
     browser.get('http://localhost:8000/#/guest-search');
   });
 
-  // Cheking main parts of page like header and url
-  describe('checking header on the page and url', function() {
+  describe('General view of page', function() {
 
-    it('i should be on the guest-search page', function(){
-      expect(browser.driver.getCurrentUrl()).toMatch("#/guest-search");
-    })
+    describe('View of tags input', function() {
 
-    it('i should see Гостьовий пошук when on the page', function(){
+      it('Then i should be on the guest-search page', function(){
+        expect(browser.driver.getCurrentUrl()).toMatch("#/guest-search");
+      })
+
+      it('And I see guest search page with header', function(){
         expect(element(by.css('h3.pageTitle')).getText()).toMatch('Гостьовий пошук');
-    });
+      });
 
-  });
+      it('And text input form with "Search" button', function(){
+        input.sendKeys('історія');
+        searchButton.click();
+        expect(element.all(by.repeater('quiz in searchResults')).count()).toEqual(2);
+      });
 
-  describe('Checking of all categories in category filter', function() {
+    }); 
+
+    describe('View of category filter menu', function() {
 
       it('i should see par-categories', function(){
         expect(element.all(by.repeater('parCat in allCats')).count()).toEqual(3);
@@ -46,44 +46,51 @@ describe('Guest search page', function() {
         expect(element.all(by.repeater('subCat in allCats')).count()).toEqual(27);
       });
 
-  });
-
-  describe('Checking search input', function() {
-
-      it('i should see two results when send україна', function(){
-        var bas = element(by.model('tags')).sendKeys("україна");
+      it('"Застосувати фільтр" button', function(){
+        applyFilterButton.click();
+        expect(element.all(by.repeater('quiz in searchResults')).count()).toEqual(2);
       });
 
+    });
+
   });
+
+  describe('View of search result', function() {
+
+    beforeEach(function() {  
+      ptor.addMockModule('httpBackendMock', mockModule.httpBackendMock);
+      browser.get('http://localhost:8000/#/guest-search');
+      input.sendKeys('історія');
+      searchButton.click();
+    });
+
+    it('I should see title of quiz', function() {
+
+      expect(firstResult.getText()).toContain('Друга світова війна');
+      expect(secondResult.getText()).toContain('Тест на знання історії світу');
+
+    });
+
+    it('I should see description of quiz', function() {
+
+      expect(firstResult.getText()).toContain('Детальний опис тесту');
+      expect(secondResult.getText()).toContain('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in fringilla quam. Donec arcu ex, egestas ut lobortis ac, tempus et nisl. Ut vulputate efficitur.');
+
+    });
+
+    it('I should see tags of quiz', function() {
+
+      expect(firstResult.getText()).toContain('| історія | світ |');
+      expect(secondResult.getText()).toContain('| світ | історія | всьо |');
+
+    });
+
+    it('When i prees on quiz title i should redirect to quiz pass page', function() {
+      element(by.css('[href="#/assessments/3"]')).click();
+      expect(browser.driver.getCurrentUrl()).toMatch("#/assessments/3");
+    });
+
+  });  
 
 });
 
-/*
-      it('should see sub-categories ', function(){
-        expect(element.all(by.repeater('subCat in allCats')).count()).toEqual(2);
-      });
-
-      // testing 1 subcategory in 1 category
-
-      it('should see first subcategory title', function(){
-        expect(element.all(by.css('.rectangle')).get(0).getText()).toMatch(subcategory1[0].title);
-      });
-
-      it('should show all quizzes of 1 subCat', function(){
-        var firstSubCat = element.all(by.css('.categories-container'));
-        var firstSubCatQuiz = firstSubCat.get(0).all(by.repeater('quizz in quizzes'));
-        var firstSubCatLength = quizzes2.length;
-        expect(firstSubCatQuiz.count()).toEqual(firstSubCatLength);
-      });
-
-      it('click on first subcategory', function(){
-        var firstSubCat = element.all(by.css('.rectangle'));
-        firstSubCat.get(0).click();
-        expect(browser.getLocationAbsUrl()).toMatch('http://localhost:8000/#/subcategory/2');
-      });
-
-      // testing 2 subcategory in 1 category
-
-
-element(by.buttonText('Реєстрація')).click();
-*/
