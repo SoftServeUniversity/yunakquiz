d44442a2df3b35af0194f740214b72534f5a2d2d
Measim <l_eugene@inbox.ru>
2014-11-23 16:28:03 +0200

    faq tab was accedantly changed , reverted back

--- source/ang-app/app/modules/administration_panel/faq_tab.js
+++ source/ang-app/app/modules/administration_panel/faq_tab.js
@@ -1,29 +1,30 @@
 (function (){
-  var  app = angular.module('yunakQuiz.faqTab' ,['ngRoute', 'xeditable', 'yunakQuiz.permission']);
+  var  app = angular.module('yunakQuiz.faqTab' ,['ngRoute', 'xeditable']);
 
-    app.config(['$routeProvider',
-      function($routeProvider) {
-        $routeProvider.
-          when('/administration-panel/faqTab', {
-            templateUrl: './modules/administration_panel/faq_tab.html',
-            controller: 'faqTab'
-          })
-      }
-    ]);
+  app.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/administration-panel/faqTab', {
+          templateUrl: './modules/administration_panel/faq_tab.html',
+          controller: 'faqTab'
+        })
+    }
+  ]);
+  
   app.run(function(editableOptions, editableThemes) {
     editableThemes.bs3.inputClass = 'input-sm';
     editableThemes.bs3.buttonsClass = 'btn-sm';
     editableOptions.theme = 'bs3';
   });
 
-  app.factory("getQuestions", ['$http', function ($http) {
+  app.factory("QuestionService", ['$http', function ($http) {
     return { 
       get : function() {
         return $http.get('http://localhost:9292/faq');
       },
 
-      post : function(data){
-        return $http.post('http://localhost:9292/saveQuestion', data);
+      put : function(data){
+        return $http.put('http://localhost:9292/saveQuestion', data);
       },
 
       delete : function(index){
@@ -31,40 +32,52 @@
       }
     }
   }]);
-    app.controller('faqTab', ['$http', 'getQuestions', '$scope', 'getAccess', '$location', function ($http, getQuestions, $scope, getAccess, $location) {
-      $scope.tab = 'Часті запитання';
+  
+  app.controller('faqTab', ['$http', 'QuestionService', '$scope', function ($http, QuestionService, $scope) {
+    $scope.tab = 'faqTab';
+    $scope.showButton = false;
 
-      getAccess($scope.tab).then(function(data){
-          if(data) {
-            getQuestions.get().success(function(data){
-            $scope.Questions = data;
-          });
-          } else {
-            $location.path( "/404" );
-          }
-        },function(){
-          $location.path( "/404" ); 
-          }
-      );
-    
-      $scope.saveQuestion = function(data, id) {
-        angular.extend(data, {id: id});
-        getQuestions.post(data);        
+    var init = function(){
+      QuestionService.get().success(function(data){
+        $scope.Questions = data;
+      }) 
+    };
+
+    $scope.saveQuestion = function(data, id) {
+      angular.extend(data, {id: id});
+      QuestionService.put(data).success(function(){
+        init();
+      });   
+      $scope.showButton = false;
+    };
+
+    $scope.removeQuestion = function(index) {
+      QuestionService.delete(index).success(function(){
+        init();
+      });
+    };      
+
+    $scope.validate = function(index){        
+      if (!$scope.Questions[index].faq_question.length  && !$scope.Questions[index].faq_answer.length) {
+        $scope.Questions.splice(index, 1);
+        return false
+      }
+      return true
+    };
+
+    $scope.cancelEction = function(){
+      $scope.showButton = false;
+    };
+
+    $scope.addQuestion = function() {
+      $scope.inserted = {
+        faq_question: '',
+        faq_answer: ''
       };
+      $scope.Questions.push($scope.inserted);
+      $scope.showButton = true;
+    };
 
-      $scope.removeQuestion = function(index) {
-        getQuestions.delete(index).success(function(data1){
-          $scope.Questions = data1;
-        });
-      };      
-
-      $scope.addQuestion = function() {
-        $scope.inserted = {
-          id: $scope.Questions.length+1,
-          faq_question: '',
-          faq_answer: ''
-        };
-        $scope.Questions.push($scope.inserted);
-      };
-   }])
+    init(); 
+  }])
 })();
