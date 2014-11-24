2b8d5f7a7daf780b45aba1b7fce38b017e894aec
OlegV <o.vitvitskyi@gmail.com>
2014-11-24 16:52:20 +0200

    Cleaned up code not related to DB

--- source/data-service/models/faq.rb
+++ source/data-service/models/faq.rb
@@ -1,4 +1,3 @@
 class Faq < ActiveRecord::Base
 
-
 end
--- source/data-service/models/result.rb
+++ source/data-service/models/result.rb
@@ -2,9 +2,4 @@ class Result < ActiveRecord::Base
   belongs_to :quiz
   belongs_to :user
 
-  def self.save(user, quiz_id, grade)
-  	@quiz = Quiz.find_by(id: quiz_id)
-  	user.results.create(quiz: @quiz, grade: grade)
-  end	
-
 end
