bac1061e607641c2f6ec032c196916bf368c3395
OlegV <o.vitvitskyi@gmail.com>
2014-11-25 15:54:53 +0200

    Added Quiz page and Quiz result page

---
+++ source/ang-app/app/modules/quiz/quiz_show_ctrl.js
@@ -0,0 +1,38 @@
+'use strict';
+/** Quiz controller  */
+angular.module('yunakQuiz.assessments')
+.controller('QuizShowCtrl', 
+  ['$scope','QuizPassService', '$routeParams', '$location', 
+  function($scope, QuizPassService, $routeParams, $location) {
+
+  /** get quiz by _id from  service  */
+  QuizPassService.getQuiz($routeParams.quiz_id)
+    .success(function(data, status, headers, config){
+      QuizPassService.quiz = data;
+      $scope.quiz = QuizPassService.quiz;
+      $scope.quiz.parentCategory = QuizPassService.quiz.category.category;
+    })
+    .error(function(data){
+      $location.path('/404/');
+    });
+
+  /** mark checked answers, and call validate function  */
+  $scope.checkAnswer = function(answer,question){
+    answer.checked = !answer.checked;
+    QuizPassService.validateQuestion(question);
+  };
+
+  /** Redirect to result-page if quiz is valid  */
+  $scope.passQuiz = function(){
+    if (QuizPassService.validateQuiz()) {
+      QuizPassService.submitQuiz()
+      .success(function(){
+        $location.path($location.path()+'/result'); 
+      })
+      .error(function(data){
+        $location.path('/404/');
+      });
+    }
+  };
+
+}]);
