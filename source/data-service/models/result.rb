54a6543d4827704fd862900884c0e3f20202f52d
bagiroff <oleg.bagiroff@gmail.com>
2014-11-26 15:25:38 +0200

    fixed response from back end, changed progress-bar style

--- source/data-service/models/result.rb
+++ source/data-service/models/result.rb
@@ -7,17 +7,17 @@ class Result < ActiveRecord::Base
   	user.results.create(quiz: @quiz, grade: grade)
   end
 
-  def self.get_result(quizzes,total_items)
+  def self.get_result(quizzes,total_items,id)
   	result = []
       quizzes.each do |quiz|
         stat_res ={}
         stat_res[:quiz_id] = quiz[:id]
         stat_res[:title] = quiz[:title]
-        stat_res[:max] = quiz.results.maximum(:grade)
-        stat_res[:tries] = quiz.results.count()
-        stat_res[:time] = quiz.results.order('rowid').last['created_at']
-        stat_res[:last] = quiz.results.order('rowid').last['grade']
-        stat_res[:average] = quiz.results.average(:grade).round(2)
+        stat_res[:max] = quiz.results.where(:user_id=>id).maximum(:grade)
+        stat_res[:tries] = quiz.results.where(:user_id=>id).count()
+        stat_res[:time] = quiz.results.where(:user_id=>id).order('rowid').last['created_at']
+        stat_res[:last] = quiz.results.where(:user_id=>id).order('rowid').last['grade']
+        stat_res[:average] = quiz.results.where(:user_id=>id).average(:grade).round(2)
         result << stat_res
       end
       {result: result, totalItems: total_items}
